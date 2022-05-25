7b388eaa735bfe10dd16bd64c6eee8fcc600be37
luca.dibello@student.supsi.ch <info@lucadibello.ch>
2022-05-25 13:19:08 +0200

    Main class to accept data as command line argument

---
+++ victim/weaponized_ping.py
@@ -0,0 +1,28 @@
+import argparse
+from modules import Pinger
+
+# using argsparse, request an ip address from the user
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--ip", help="IP address to ping", required=True)
+args = parser.parse_args()
+
+# main method
+def main():
+    # instantiate the Pinger class
+    pinger = Pinger(args.ip)
+    # call the ping method
+    pinger.ping()
+
+if __name__ == "__main__":
+  # Validate IP address set by the user, if valid, execute main, otherwise print error and exit
+  try:
+    ip = args.ip
+    ip = ip.split('.')
+    if len(ip) != 4:
+      raise ValueError("Invalid IP address")
+    for i in ip:
+      if not i.isdigit():
+        raise ValueError("Invalid IP address")
+    main()
+  except ValueError as e:
+    print(e)
